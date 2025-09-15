a6027f9f486ce49b52922950431f07535efa6c9d
Yaraslau Lazakovich <ylazakovich@gmail.com>
2025-09-15 09:55:29 +0200

    [environment]: part 1 - start init

---
+++ tools/scripts/dev/stop_app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker compose \
+  -f tools/docker/docker-compose.yml \
+   down -v
---
+++ tools/scripts/mock/run_mock_server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "🚀 Starting MockServer container..."
+docker compose \
+  -f tools/docker/docker-compose.yml \
+   up -d --quiet-pull mock-server
+
+sleep=2
+max_count=30
+count=0
+status=0
+
+while true; do
+  status=$(curl -o /dev/null -s -w "%{http_code}" http://localhost:1080/mockserver/dashboard)
+
+  if [[ "$status" -eq 200 ]]; then
+    echo "✅ MockServer container started successfully"
+    break
+  fi
+
+  echo "⏳ Waiting for MockServer... ${count}s elapsed, status code: $status"
+  sleep "$sleep"
+  count=$((count + sleep))
+
+  if [[ "$count" -gt "$max_count" ]]; then
+    echo -e "\e[1;31m❌ MockServer did not start within ${max_count}s (http://localhost:1080)\e[0m"
+    exit 1
+  fi
+done
