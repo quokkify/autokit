962477637a565516f5197ec641b8d43ce0435df0
Yaraslau Lazakovich <ylazakovich@gmail.com>
2025-12-17 13:39:48 +0100

    feature: create config for TestNG extension

--- testng-extensions/src/main/java/io/automation/config/TestNGExtension.java
+++ testng-extensions/src/main/java/io/automation/config/TestNGExtension.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Mutable;
 import org.aeonbits.owner.Reloadable;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
-@Config.Sources({"system:env"})
+@Config.Sources({"system:env", "classpath:testng.properties"})
 public interface TestNGExtension extends Config, Reloadable, Mutable {
 
   @Key("RETRY_COUNT")
@@ -14,6 +14,10 @@ public interface TestNGExtension extends Config, Reloadable, Mutable {
 
   @Key("TEST_GROUP")
   String testGroup();
+
+  @Key("SUITE_NAME")
+  @DefaultValue("Default suite")
+  String suiteName();
 
   @Key("EXECUTION_MODE")
   @DefaultValue("LOCAL")
