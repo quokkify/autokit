92a001e629bafbcfbc1697fbec415e91cf3e8d2c
Yaraslau Lazakovich <ylazakovich@gmail.com>
2026-02-01 11:35:48 +0100

    ench: enabled mongo module (#135)

--- tools/environment/scripts/infra/stop_app.sh
+++ tools/environment/scripts/infra/stop_app.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+set -euo pipefail
 COMPOSE_FILES=(-f tools/environment/docker/docker-compose.yml)
 if [[ "${CI:-}" == "true" ]]; then
   COMPOSE_FILES+=(-f tools/environment/docker/docker-compose.ci.yml)
+fi
+
+PROFILE="${1:-all}"
+
+PROFILES_ARGS=()
+if [[ "$PROFILE" != "none" && -n "$PROFILE" ]]; then
+  if [[ "$PROFILE" == "all" ]]; then
+    PROFILES_ARGS=(--profile web --profile messaging --profile mock --profile realtime --profile storage)
+  else
+    IFS=',' read -ra TOKENS <<<"$PROFILE"
+    for profile in "${TOKENS[@]}"; do
+      profile="$(echo "$profile" | xargs)" # trim
+      [[ -n "$profile" ]] && PROFILES_ARGS+=("--profile" "$profile")
+    done
+  fi
 fi
 
 DOWN_ARGS=(down)
@@ -11,4 +27,5 @@ fi
 
 docker compose \
   "${COMPOSE_FILES[@]}" \
+  "${PROFILES_ARGS[@]}" \
   "${DOWN_ARGS[@]}"
