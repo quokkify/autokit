7a57d22356c020f56243c57c0937d82197be4032
Yaraslau Lazakovich <ylazakovich@gmail.com>
2026-02-01 13:56:41 +0100

    change host

--- tools/environment/scripts/infra/run_app.sh
+++ tools/environment/scripts/infra/run_app.sh
@@ -70,7 +70,7 @@ for profile in "${TOKENS[@]}"; do
       fi
       host="localhost"
       if [[ "${CI:-}" == "true" ]]; then
-        host="nginx"
+        host="dind"
       fi
       echo "NGINX_BASE_URL=http://${host}:${port}" > tools/environment/.nginx.env
       ;;
